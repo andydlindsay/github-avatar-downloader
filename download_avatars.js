c284e18608c55f3272e88df7c153bf51efa91afb
Andy Lindsay <andydlindsay@gmail.com>
2018-10-29 18:41:48 +0000

    added basic request structure

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,20 @@
-var request = require('request');
+const request = require('request');
+const https = require('https');
+const log = console.log;
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  const url = `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`;
+  request(url, (err, res, body) => {
+    cb(err, body);
+  });
+}
+
+const repoOwner = 'jquery';
+const repoName = 'jquery';
+
+getRepoContributors(repoOwner, repoName, (err, result) => {
+  log("Errors:", err);
+  log("Result:", result);
+});

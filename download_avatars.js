2d5f01c40ff44d8dc351689305735ddc7d661635
Andy Lindsay <andydlindsay@gmail.com>
2018-10-30 00:52:38 +0000

    protected against bad credentials

--- download_avatars.js
+++ download_avatars.js
@@ -17,6 +17,10 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
   request(options, (err, res, body) => {
+    if (JSON.parse(res.body).message) {
+      log('Bad Github credentials');
+      return;
+    }
     cb(err, JSON.parse(body));
   });
 }
@@ -36,19 +40,24 @@ const repoName = process.argv[3];
 if (repoOwner && repoName) {
   log('Welcome to the GitHub Avatar Downloader!');
   getRepoContributors(repoOwner, repoName, (err, result) => {
-    log("Errors:", err);
-    // create the avatars directory if it does not already exist
-    mkdirp('./avatars', (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      } else {
+    if (err) {
+      throw err;
+    }
+    if (result.length) {
+      // create the avatars directory if it does not already exist
+      mkdirp('./avatars', (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
         result.forEach((contributor) => {
           downloadImageByURL(contributor.avatar_url, `avatars/${contributor.login}`);
         });
         log('Result: Download Complete');
-      }
-    });
+      });
+    } else {
+      log('Specified repo does not exist');
+    }
   });
 } else {
   log('USAGE: node download_avatars.js <repoOwner> <repoName>');
